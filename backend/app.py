faa684c51370ae0b01cc2aa81f35a0ca59e7169d
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-15 11:39:59 +0000

    Add add/get/del for todos

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,70 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+class Todo(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(index=True)
+    author: str = Field(index=True)
+    description: str = Field(index=True)
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
 
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/todos/")
+def read_todos(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[Todo]:
+    todos = session.exec(select(Todo).offset(offset).limit(limit)).all()
+    return todos
+
+@app.post("/todos/")
+def create_todo(todo: Todo, session: SessionDep) -> Todo:
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+    return todo
+
+@app.get("/todos/{todo_id}")
+def read_todo(todo_id: int, session: SessionDep) -> Todo:
+    todo = session.get(Todo, todo_id)
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return todo
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int, session: SessionDep):
+    todo = session.get(Todo, todo_id)
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    session.delete(todo)
+    session.commit()
+    return {"ok": True}
